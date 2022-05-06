d32b1d8aafff4133a7c52c45492161d85245561d
Gnezd <ycdzeng@gmail.com>
2022-05-06 12:09:49 +0800

    1. Deprecate sum_up, replace with plot_map after Scan.load. 2. Headder detection 3. Take out process.rb 4. Segment test units 5. FFT test

--- lib.rb
+++ lib.rb
@@ -8,77 +8,53 @@
 # Todo: accecpt code block for summing. Tricky: injection to whole loop
 # In general, mode extraction can be representated as a matrix acting on a spectrum but this requires spectrum spectrum constructing and lacks the freedom of passing blocks and conditions
 # An even more general way of doing things is to pass into sum_up() a list of criteria on the wavelengths
-def sum_up(scan)
-  (path, scan_name, width, height, depth) = scan
-  raise "wrong number of arguments" unless scan.size == 5
-  fin = File.open path, 'r'
-  lines = fin.readlines
-  fin.close
-  
-  # Detection of seperator , or tab
-  raiese "No seperator found in line 0." unless match = lines[0].match(/[\t,]/)
-  seperator = match[0]
 
-  # Extract units from headderline
-  if lines[0] =~ /^Frame\t/
-    headder_line = lines.shift
-    (unit_x, unit_y) = headder_line.split(seperator)[1..2]
+def plot_map(scan, sum = nil)
+  if sum == nil
+    sum = Proc.new {|spect| (spect.map{|pt| pt[1]}).sum}
   end
-
-  raise "Number of lines is #{lines.size}, not multiplication of width(#{width}) * height(#{height}) * depth(#{depth})!" unless lines.size % (width * height * depth) == 0
-
-  puts "Processing summation of '#{scan_name}' from '#{path}'"
-  puts "Got #{lines.size} lines of spectrum to sum up."
-
-
-  sum = Array.new(width * height * depth) {0.0}
-  (0..lines.size-1).each do |ln|
-    (frame, wv, intensity) = lines[ln].split seperator
-    frame = frame.to_i - 1 # Matter of convention. I count up from 0.
-    wv = wv.to_f
-    intensity = intensity.to_f
-
-    # Summing, the easiest version
-    raise "at frame #{frame} / #{lines[ln].split "\t"}" unless sum[frame]
-    sum[frame] += intensity # Full sum
-  end
-
-  filenames = []
-  (0..depth-1).each do |z| # Each depth one graph
-    puts "outputing z = #{z}."
-    fout = File.open scan_name + "_#{z}.tsv", 'w'
-    (0..height-1).each do |row|
-      fout.puts sum[row * width + z * width * height .. (row + 1) * width + z * width * height - 1].join "\t"
+  maps = []
+  # Iterate through z layers
+  (0..scan.depth-1).each do |z|
+    map = Array.new(scan.width) {Array.new(scan.height) {0.0}} # One slice
+    (0..scan.width-1).each do |x|
+      (0..scan.height-1).each do |y|
+        map[x][y] = sum[scan[x][y][z]]
+      end
     end
-    filenames.push scan_name + "_#{z}"
-    fout.close
-  end
-  filenames
-end
 
-def plot_map(scan_name, width, height)
-  gplot = File.open 'gplot.temp', 'w'
+    # Export map and push
+    map_fout = File.open "output/#{scan.name}_#{z}.tsv", 'w'
+    map.transpose.each do |row|
+      map_fout.puts row.join "\t"
+    end
+    map_fout.close
+    maps.push map
+
+    # Plotting
+    gplot = File.open "output/#{scan.name}_#{z}.gplot", 'w'
 gplot_content =<<GPLOT_HEAD
-set terminal svg size #{width * 5},#{height * 5} mouse enhanced standalone
-set output '#{scan_name}.svg'
+set terminal svg size #{scan.width * 5},#{scan.height * 5} mouse enhanced standalone
+set output 'output/#{scan.name}_#{z}.svg'
 set border 0
 unset key
 unset xtics
 unset ytics
-set xrange[-0.5:#{width}-0.5]
-set yrange[-0.5:#{height}-0.5]
-set title '#{scan_name.sub('_','\_')}'
+set xrange[-0.5:#{scan.width}-0.5]
+set yrange[-0.5:#{scan.height}-0.5]
+set title '#{scan.name.gsub('_','\_')}-#{z}'
 unset colorbox
 set palette cubehelix negative
-plot '#{scan_name}.tsv' matrix with image pixels
-set terminal png size #{width * 5},#{height * 5}
-set output '#{scan_name}.png'
+plot 'output/#{scan.name}_#{z}.tsv' matrix with image pixels
+set terminal png size #{scan.width * 5},#{scan.height * 5}
+set output 'output/#{scan.name}_#{z}.png'
 replot
 GPLOT_HEAD
-  gplot.puts gplot_content
-  gplot.close
-  puts "Plotting #{scan_name}, W: #{width}, H: #{height}"
-  `gnuplot gplot.temp`
+    gplot.puts gplot_content
+    gplot.close
+    puts "Plotting #{scan.name}_#{z}, W: #{scan.width}, H: #{scan.height}"
+    `gnuplot output/#{scan.name}_#{z}.gplot`
+  end
 end
 
 # Return list of points in rectangular area defined by pt_1(x,y) to pt_2(x,y)
@@ -98,7 +74,7 @@ end
 
 class Scan < Array
   # Assume all wavelength scales allign across all pixels
-  attr_accessor :frames, :wv, :path
+  attr_accessor :frames, :wv, :spectrum_units,:path, :name, :width, :height, :depth
   def initialize (path, name, width, height, depth)
     @path = path
     @name = name
@@ -114,15 +90,32 @@ class Scan < Array
     fin = File.open @path, 'r'
     puts "Reading #{@path}..."
     lines = fin.readlines
-    lines.shift if lines[0] =~ /^Frame[\t,]/
-    raise "Number of lines is #{lines.size}, not multiplication of width * height * depth!" unless lines.size % (@width * @height * @depth) == 0
-    @spectral_width = lines.size / (@width * @height * @depth)
-    puts "Got #{lines.size} lines of spectrum to process."
+    fin.close
 
     # Detection of seperator , or tab
     raiese "No seperator found in line 0." unless match = lines[0].match(/[\t,]/)
     seperator = match[0]
 
+    # Detection of title line and units
+    if lines[0] =~ /^Frame[\t,]/
+      title_line = lines.shift
+      @spectrum_units = title_line.split(seperator)[1..2]
+      puts "Title line detected. Unit identified as #{@wv_unit}"
+    else
+      puts "No title line detected. Please ensure data format to be <Frame #> <Wavelength/wavenumber> <Intensity>"
+      puts "And input <wavelength/wavenumber, intensity> unit below:"
+      @wv_unit = gets.chomp.split /, ?/
+    end
+
+    # Framesize and wavelength construction
+    @framesize = (lines.index {|ln| ln[0] == '2'})
+    @wv = (0.. @framesize -1).map {|i| lines[i].split(seperator)[1]}
+    puts "Frame size determined to be #{@framesize}, spectral range being #{@wv[0]} .. #{@wv[-1]}"
+    
+    raise "Number of lines is #{lines.size}, not multiplication of given width (#{@width}) * height (#{@height})* depth (#{@depth})!" unless lines.size == @framesize * (@width * @height * @depth)
+    puts "Got #{lines.size} lines of spectrum to process."
+
+    # The real parsing
     lines.each do |line|
       (frame, wv, intensity) = line.split seperator
       frame = frame.to_i - 1 # Matter of convention. I count up from 0.
@@ -133,17 +126,33 @@ class Scan < Array
       i = (frame % (@width * @height)) % @width
       self[i][j][k].push [wv, intensity]
     end
+
+    # Update all spectra
+    puts "Loading done. Updating info of spectra."
+    self.each do |row|
+      row.each do |column|
+        column.each do |pixel|
+      pixel.units = @spectrum_units
+      pixel.update_info
+        end
+      end
+    end
+
     @loaded = true
     puts "done"
   end
 
   def extract_spect(points)
     raise "Not a series of points input." unless (points.is_a? Array) && (points.all? {|item| item.size == 3})
+    if !@loaded
+      puts "Scan #{@name} not yet loaded. Loading."
+      self.load
+    end
     result = []
     points.each do |pt|
       result.push self[pt[0]][pt[1]][pt[2]]
       result.last.name = pt.join '-'
-    end
+    end 
     result
   end
 
@@ -192,10 +201,37 @@ class Spectrum < Array
     @spectral_range = self.minmax_by { |pt| pt[0] }.map{ |pt| pt[0] }
     @signal_range = self.minmax_by { |pt| pt[1] }.map{ |pt| pt[1] }
   end
+
+  def ma(radius)
+    # +- radius points moving average
+    # Issue: Radius defined on no of points but not real spectral spread 
+    raise "Radius should be integer but was given #{radius}" unless radius.is_a?(Integer)
+    raise "Radius larger than half the length of spectrum" if 2*radius >= self.size
+
+    result = Array.new(self.size - 2 * radius)
+    result.each_index do |i|
+      # Note that the index i of the moving average chromatogram aligns with i + radius in originaal chromatogram
+      x = self[i + radius][0]
+      y = 0.0
+      (i .. i + 2 * radius).each do |origin_i|
+        # Second loop to run through the neighborhood in origin
+        # Would multiplication with a diagonal stripe matrix be faster than nested loop? No idea just yet.
+        y += self[origin_i][1]
+      end
+      y = y / (2 * radius + 1) # Normalization
+      result[i] = [x, y]
+    end
+    result
+  end
 end
 
 def plot_spectra(spectra)
   raise "Not an array of spectra input." unless (spectra.is_a? Array) && (spectra.all? Spectrum)
+
+  # Check if they align in x_units
+  x_units = spectra.map {|spectrum| spectrum.units[0]}
+  raise "Some spectra have different units!" unless x_units.all? {|unit| unit == x_units[0]}
+
   plotdir = "plot-" + Time.now.strftime("%d%b-%H%M%s")
   Dir.mkdir plotdir
   plots = []
@@ -208,7 +244,7 @@ def plot_spectra(spectra)
   plot_headder = <<GPLOT_HEADER
   set terminal png size 800,600 lw 2
   set output '#{plotdir}/spect_plot.png'
-  set xlabel 'wavelength (nm)'
+  set xlabel '#{x_units[0]}'
   set ylabel 'intensity (cts)'
 GPLOT_HEADER
   gplot.puts plot_headder
--- test.rb
+++ test.rb
@@ -1,50 +1,55 @@
 require './lib.rb'
 require 'gsl'
-=begin
-require 'benchmark'
-puts "Starting time: #{Time.now}"
-scan = Scan.new('18Apr-2566-5-scan/2566-5-survey-z0.csv', '2566-5-survey', 90, 90, 1)
-puts "Starting to load: #{Time.now}"
-scan.load
 
-puts "Starting to exise and plot: #{Time.now}"
-#Excise
-spects = []
-(5..20).each do |y|
-  spects.push scan[63][y][0]
-  spects.last.name = "68_#{y}_0"
-end
-plot_spectra(spects)
-puts "Done at #{Time.now}"
+def loading_test()
+  require 'benchmark'
+  puts "Starting time: #{Time.now}"
+  scan = Scan.new('testdata/csv', 'testdata', 45, 45, 3)
+  puts "Starting to load: #{Time.now}"
+  scan.load
 
-scale = GSL::Vector.linspace(0,1,2048)
-data = GSL::Sf::sin(2*GSL::M_PI*scale*10) + 0.5 * GSL::Sf::sin(2*GSL::M_PI*scale*20)
-y = data.fft
-scan = Scan.new('testdata/64-84.9-w15h15d5-45x45x3 10_36_52 microPL.csv', 'scan1', 45, 45, 3)
-scan.load
-=end
-
-
-plotlines = []
-# Across a linescan, skipping last pixel
-[0, 7, 12, 15, 29].each_with_index do |x, i|
-  spect = Spectrum.new("output/#{x}-spect.tsv")
-  plotlines.push "'output/#{x}-spect.tsv' with lines t 'spect #{x}' lt #{i % 8 +1}"
-
-  #ft = GSL::Vector.alloc(scan[x][23][0].map{|pt| pt[1]}).fft # 究極一行文
-  ft = GSL::Vector.alloc(spect.map{|pt| pt[1]}).fft # 究極一行文
-  fout = File.new "output/#{x}-ft.tsv", 'w'
-  ft = (ft * ft).normalize # Be positive
-  ft.each_index do |i|
-    fout.puts "#{i}\t#{ft[i]}"
+  puts "Starting to exise and plot: #{Time.now}"
+  #Excise
+  spects = []
+  (5..20).each do |y|
+    spects.push scan[24][y][0]
+    spects.last.name = "68_#{y}_0"
   end
-  plotlines.push "'output/#{x}-ft.tsv' with lines t 'ft #{x}' axes x2y2 lt #{i % 8 + 1}"
-  fout.close
-
+  plot_spectra(spects)
+  puts "Done at #{Time.now}"
 end
-plotline = "plot " + plotlines.join(", \\\n")
 
-ft_plot_directive = <<GPLOT
+def ft_test
+  scale = GSL::Vector.linspace(0,1,2048)
+  data = GSL::Sf::sin(2*GSL::M_PI*scale*10) + 0.5 * GSL::Sf::sin(2*GSL::M_PI*scale*20)
+  y = data.fft
+  scan = Scan.new('testdata/64-84.9-w15h15d5-45x45x3 10_36_52 microPL.csv', 'scan1', 45, 45, 3)
+  scan.load
+  (0..44).each do |x|
+    scan[x][23][0].write_tsv "output/#{x}-spect.tsv"
+  end
+
+  plotlines = []
+  # Across a linescan, skipping last pixel
+  (0..44).each_with_index do |x, i|
+    next unless i % 5 == 0
+    spect = Spectrum.new("output/#{x}-spect.tsv")
+    plotlines.push "'output/#{x}-spect.tsv' with lines t 'spect #{x}' lt #{i % 8 +1}"
+
+    #ft = GSL::Vector.alloc(scan[x][23][0].map{|pt| pt[1]}).fft # 究極一行文
+    ft = GSL::Vector.alloc(spect.map{|pt| pt[1]}).fft # 究極一行文
+    fout = File.new "output/#{x}-ft.tsv", 'w'
+    ft = ft.to_complex2.abs # Be positive
+    ft.each_index do |i|
+      fout.puts "#{i}\t#{ft[i]}"
+    end
+    plotlines.push "'output/#{x}-ft.tsv' u 1:($2) with lines t 'ft #{x}' axes x2y2 lt #{i % 8 + 1}"
+    fout.close
+
+  end
+  plotline = "plot " + plotlines.join(", \\\n")
+
+  ft_plot_directive = <<GPLOT
 set terminal svg size 800,600 mouse enhanced standalone
 set linetype 1 lc rgb "black"
 set linetype 2 lc rgb "dark-red"
@@ -57,12 +62,46 @@ set linetype 8 lc rgb "dark-violet"
 set linetype cycle 8
 set output 'output/ft.svg'
 set title 'FT'
+set ylabel 'Spectrum counts'
+set y2label 'Normalized FFT intensity'
 set y2tics
 set x2tics
-set x2range [2:50]
+set yrange [0:*]
+#set y2range [0:*]
+set x2range [1:50]
 GPLOT
-gplot_out = File.open 'output/fft.gplot', 'w'
-gplot_out.puts ft_plot_directive
-gplot_out.puts plotline
-gplot_out.close
-system 'gnuplot output/fft.gplot'
+  gplot_out = File.open 'output/fft.gplot', 'w'
+  gplot_out.puts ft_plot_directive
+  gplot_out.puts plotline
+  gplot_out.close
+  system 'gnuplot output/fft.gplot'
+
+end
+
+def test_fft_map
+scan = Scan.new './testdata/csv', 'Test_fft_10_20_sum', 45, 45, 3
+scan.load
+fft1 = Proc.new{|spect| 
+  ft = GSL::Vector.alloc(spect.map{|pt| pt[1]}).fft # 究極一行文
+  ft = ft.to_complex2.abs # Be positive
+  ft[9..19].sum
+}
+plot_map scan, fft1
+scan.name = 'Test_simple_sum'
+plot_map scan
+end
+
+def fitting_test
+  spect = Spectrum.new 'output/10-spect.tsv'
+  puts spect.size
+  ma20 = spect.ma(20)
+  v = GSL::Vector.alloc spect.map{|pt| pt[1]}
+
+  fout = File.open 'output/fit.tsv', 'w'
+  (0..v.size-1).each do |i|
+    fout. puts "#{i}\t #{v[i]}"
+  end
+  fout.close
+end
+
+loading_test
