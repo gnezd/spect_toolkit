7a6c6f3139b9ce9125d179a2863963243ef26184
YC Dzeng <ycdzeng@gmail.com>
2026-03-02 11:38:10 +0800

    SpectCache now deserializes to Spectrum using @meta, and handles tracks and image data types

--- src/spectcache.rb
+++ src/spectcache.rb
@@ -6,23 +6,25 @@ class SpectCache
   def initialize(cache, name, options = {}) # optional data input determins init mode
     @cache = cache
     raise "Not a Memcached client!" unless @cache.is_a? Memcached::Client
+    @options = options
     @name = name
-    @meta = options[:meta]
-    @data = options[:data]
-    @wv = options[:wv]
-    if @data # Only case that warrants a write at init would be data-containing new()
+    if @options[:data] # Only case that warrants a write at init would be data-containing new()
       puts "Data input at init, writing cache"
       write_cache
+    else
+      read_cache
     end
     self 
   end
 
   def read_cache
     @meta = JSON.parse(@cache.get("spect_meta_" + @name)).transform_keys {|k| k.to_sym}
-    if @meta[:wv_ref]
-      @wv = @cache.get("spect_wv_#{meta[:wv_ref]}").unpack("#{meta[:wv_type]}*")
+
+    # Assuming that all rois will have the same @wv for now
+    if @meta[:spect_meta]&.[]('wv_ref')
+      @wv = @cache.get("spect_wv_#{@meta[:spect_meta]['wv_ref']}").unpack("#{@meta[:spect_meta]['wv_type']}*")
     else
-      @wv = @cache.get("spect_wv_#{@name}").unpack("#{meta[:wv_type]}*")
+      @wv = @cache.get("spect_wv_#{@name}").unpack("#{@meta[:spect_meta]['wv_type']}*")
     end
 
     begin
@@ -31,15 +33,36 @@ class SpectCache
       time = nil
     end
     @meta[:time] = time
+    @meta[:spect_meta].transform_keys!(&:to_sym)
 
-    @data = @cache.get("spect_#{@name}").unpack("#{meta[:type]}*")
+    raw_data = @cache.get("spect_#{@name}").unpack("#{@meta[:spect_meta][:type]}*")
+    case @meta[:acq_type]
+    when 'spectrum'
+      @data = Spectrum.new
+      @data.meta = @meta[:spect_meta]
+      @data.wv = @wv if @wv.size > 0
+      @data.signal = raw_data
+      @data.cache_host = @cache
+    when 'tracks'
+      @data = Array.new(@meta[:tracks].size) {Spectrum.new}
+      @data.each_with_index do |spect, i|
+        spect.meta = @meta[:spect_meta].transform_keys {|k| k.to_sym} 
+        spect.wv = @wv if @wv.size > 0
+        spect.signal = raw_data[i*@wv.size .. (i+1)*@wv.size - 1]
+        spect.cache_host = @cache
+      end
+    when 'image'
+      width = (@meta[:ImageROI]["hend"] - @meta[:ImageROI]["hstart"] + 1) / @meta[:ImageROI]["hbin"]
+      height = (@meta[:ImageROI]["vend"] - @meta[:ImageROI]["vstart"] + 1) / @meta[:ImageROI]["vbin"]
+      @data = (0..height-1).map {|y| raw_data[y*width .. (y+1)*width - 1]}
+    end
     true
   end
 
   def write_cache
     # Determine datafield type. Save space and time on integers! Default to double.
-    type = @meta[:type] ? @meta[:type] : 'D'
-    wvtype = @meta[:wv_type] ? @meta[:wv_type] : 'D'
+    type = @meta[:spect_meta][:type] ? @meta[:spect_meta][:type] : 'D'
+    wvtype = @meta[:spect_meta][:wv_type] ? @meta[:spect_meta][:wv_type] : 'D'
     @cache.set "spect_" + @name, @data.pack("#{type}*")
     @cache.set "spect_meta_" + @name, @meta.to_json
 
@@ -53,11 +76,6 @@ class SpectCache
 
   def to_spectrum
     read_cache
-    spect = Spectrum.new
-    spect.meta = @meta
-    spect.wv = @wv if @wv.size > 0
-    spect.signal = @data
-    spect.cache_host = @cache
 
     spect
   end
